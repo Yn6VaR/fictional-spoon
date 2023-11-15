38e400a60c2d39e94fb3d3efa7a6b932a0cbf6d8
Yn6VaR <150354548+Yn6VaR@users.noreply.github.com>
2023-11-15 18:17:56 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,8 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 missing_index = 4
 numbers[missing_index] = 0
-unique_numbers = set(numbers)
-sum_of_numbers = sum(set(numbers))
 count_of_numbers = len(numbers)
 
 average_of_numbers = sum(set(numbers)) / len(numbers)
