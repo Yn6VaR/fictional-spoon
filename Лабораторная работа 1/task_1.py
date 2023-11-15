a01f4feea118f6d7cc21c7a9d60e4141350352c7
Yn6VaR <150354548+Yn6VaR@users.noreply.github.com>
2023-11-15 18:11:12 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,12 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-numbers[4] = 0
+missing_index = 4
+numbers[missing_index] = 0
 unique_numbers = set(numbers)
 sum_of_numbers = sum(set(numbers))
 count_of_numbers = len(numbers)
 
-average_of_numbers = sum(set(numbers)) / len(numbers) # TODO заменить значение пропущенного элемента средним арифметическим
+average_of_numbers = sum(set(numbers)) / len(numbers)
 
-numbers[4] = average_of_numbers
+numbers[missing_index] = average_of_numbers
 
 print("Измененный список:", numbers)
